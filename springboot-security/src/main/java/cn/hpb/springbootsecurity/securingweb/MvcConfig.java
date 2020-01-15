75496ea02cfc90b40d6ad45910417b23e70113e5
hanpengbo <hpbjob@163.com>
2020-01-15 15:48:04 +0800

    登录 登出  基础功能

---
+++ springboot-security/src/main/java/cn/hpb/springbootsecurity/securingweb/MvcConfig.java
@@ -0,0 +1,19 @@
+package cn.hpb.springbootsecurity.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author hanpengbo
+ * @Date 2020/1/15 11:16
+ **/
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
